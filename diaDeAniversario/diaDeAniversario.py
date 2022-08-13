9b64df0d538cf342deffd73f2d9a34228964ef11
Luís Gustavo <54186119+luisgustavo02@users.noreply.github.com>
2022-08-13 08:43:42 -0300

    Create diaDeAniversario.py

---
+++ diaDeAniversario/diaDeAniversario.py
@@ -0,0 +1,16 @@
+print("1\t3\t5\t7\t9\t11\t13\t15\t17\t19\t21\t23\t25\t27\t29\t31")
+n1 = int(input("O DIA DO SEU ANIVERSÁRIO ESTÁ LISTADO ACIMA? (RESPONDA 1 PARA SIM E 0 PARA NÃO) "))
+
+print("2\t3\t6\t7\t10\t11\t14\t15\t18\t19\t22\t23\t26\t27\t30\t31")
+n2 = int(input("O DIA DO SEU ANIVERSÁRIO ESTÁ LISTADO ACIMA? (RESPONDA 1 PARA SIM E 0 PARA NÃO) "))
+
+print("4\t5\t6\t7\t12\t13\t14\t15\t20\t21\t22\t23\t28\t29\t30\t31")
+n3 = int(input("O DIA DO SEU ANIVERSÁRIO ESTÁ LISTADO ACIMA? (RESPONDA 1 PARA SIM E 0 PARA NÃO) "))
+
+print("8\t9\t10\t11\t12\t13\t14\t15\t24\t25\t26\t27\t28\t29\t30\t31")
+n4 = int(input("O DIA DO SEU ANIVERSÁRIO ESTÁ LISTADO ACIMA? (RESPONDA 1 PARA SIM E 0 PARA NÃO) "))
+
+print("16\t17\t18\t19\t20\t21\t22\t23\t24\t25\t26\t27\t28\t29\t30\t31")
+n5 = int(input("O DIA DO SEU ANIVERSÁRIO ESTÁ LISTADO ACIMA? (RESPONDA 1 PARA SIM E 0 PARA NÃO) "))
+
+print(f"O seu aniversário é {n1*2**0 + n2*2**1 + n3*2**2 + n4*2**3 + n5*2**4}")
